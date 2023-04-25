ccef64ac71cb0d9c528c4d1f122e6621f0df9f77
tianfuzhiguo <49770448+tianfuzhiguo@users.noreply.github.com>
2023-04-25 12:18:50 +0800

    Update InitConfig.py

--- common/init/InitConfig.py
+++ common/init/InitConfig.py
@@ -1,4 +1,5 @@
 import configparser
+import os.path
 
 '''
 @author: dujianxiao
@@ -14,30 +15,9 @@ class InitConfig:
         """
         try:
             # 读取conf.ini中的用户自定义变量
-            userParams = []
-            userParamsValue = []
-            fileData = []
             config = configparser.ConfigParser()
-            config.read(path + '/conf.ini', encoding="utf-8")
-            # 预置3个数据库
-            DB1 = config.get("section", "DB1")
-            DB2 = config.get("section", "DB2")
-            DB3 = config.get("section", "DB3")
-            fileData.append(DB1)
-            fileData.append(DB2)
-            fileData.append(DB3)
-            with open(path + '/conf.ini', encoding='utf-8') as f:
-                for line in f:
-                    line = line.strip('\n')
-                    if 0 < len(line) < 9:
-                        if line[0] != '#':
-                            userParams.append(line[0:line.find('=')])
-                            userParamsValue.append(line[line.find('=') + 1:])
-                    elif len(line) >= 9:
-                        if line[0] != '#' and line[0:9] != '[section]':
-                            userParams.append(line[0:line.find('=')])
-                            userParamsValue.append(line[line.find('=') + 1:])
-            return fileData, userParams, userParamsValue
+            config.read(os.path.join(path, 'conf.ini'), encoding="utf-8")
+            return config.items('section')
         except Exception as e:
             self.consoleFunc('red', '初始化conf.ini失败:')
             self.consoleFunc('black', str(e))
