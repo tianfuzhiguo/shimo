5919957daf9e813f66f16a5f5eed2687bba6401a
tianfuzhiguo <49770448+tianfuzhiguo@users.noreply.github.com>
2023-04-25 11:01:29 +0800

    Update Template.py

--- common/excel/Template.py
+++ common/excel/Template.py
@@ -28,11 +28,11 @@ class Template(Init):
         msg = self.verKeyWordExist(fileRes, sheet)
         blue = self.setCellStyle(7)
         if '未找到关键字' in str(msg):
-            self.consoleFunc('green', sheetName + ':', 'size=4')
+            self.consoleFunc('green', f'{sheetName}:', 'size=4')
             self.consoleFunc('red', str(msg))
             return msg
         elif '存在重复的关键字' in str(msg):
-            self.consoleFunc('green', sheetName + ':', 'size=4')
+            self.consoleFunc('green', f'{sheetName}:', 'size=4')
             self.consoleFunc('red', str(msg))
             return msg
         elif '关键字顺序不正确' in str(msg):
@@ -45,7 +45,7 @@ class Template(Init):
                 self.setValueColor(sheetRes, 2, msg[1], self.getValue(fileRes, sheet, 1, msg[1]), "blue")
                 self.setValueColor(sheetRes, 2, msg[2], self.getValue(fileRes, sheet, 1, msg[2]), "blue")
             bookRes.save(fileRes)
-            self.consoleFunc('green', sheetName + ':', 'size=4')
+            self.consoleFunc('green', f'{sheetName}:', 'size=4')
             self.consoleFunc('red', str(ss))
             return msg
         else:
@@ -59,7 +59,7 @@ class Template(Init):
                         self.setValueColor(sheetRes, 2, int(info[i]), self.getValue(fileRes, sheet, 1, int(info[i])),
                                            "blue")
                 bookRes.save(fileRes)
-                self.consoleFunc('green', sheetName + ':', 'size=4')
+                self.consoleFunc('green', f'{sheetName}:', 'size=4')
                 self.consoleFunc('red', str(info))
                 return info
             else:
@@ -71,20 +71,15 @@ class Template(Init):
         :param file:用例文件
         :param sheet:用例文件
         """
-
-        # 定义关键字数组
-        arr = ['name', 'url', 'method', 'param', 'file', 'header', 'part101', 'part201', 'part301', 'section101',
-               'section201',
-               'section301', 'resText', 'resHeader', 'statusCode', 'expression', 'status', 'time', 'init001',
-               'restore001', 'dyparam001',
-               'key001', 'value001', 'headerManager', '数据库', 'Iteration']
         arrCopy = []
         order = []
-        msg = []
-        sts = "未找到关键字"
-        msg.append(sts)
         keyWord = ''
+        msg = ["未找到关键字"]
         repeat = ['存在重复的关键字']
+        # 定义关键字数组
+        arr = ['name', 'url', 'method', 'param', 'file', 'header', 'part101', 'part201', 'part301', 'section101',
+               'section201', 'section301', 'resText', 'resHeader', 'statusCode', 'expression', 'status', 'time',
+               'init001', 'restore001', 'dyparam001', 'key001', 'value001', 'headerManager', '数据库', 'Iteration']
         for item in arr:
             for i in range(self.ncols):
                 try:
@@ -110,9 +105,7 @@ class Template(Init):
         if len(msg) > 1:
             return msg
         # 找到全部关键字则校验顺序
-        msg = []
-        st = "关键字顺序不正确"
-        msg.append(st)
+        msg = ["关键字顺序不正确"]
         for i in range(len(order)):
             try:
                 if i + 1 < len(order) and order[i + 1] < order[i]:
@@ -126,9 +119,7 @@ class Template(Init):
         """
         校验各部分数量是否一致
         """
-        msg = []
-        st = '数量不一致'
-        msg.append(st)
+        msg = ["数量不一致"]
         # 校验字段
         len1 = self.getArrLenth(self.part101Col, self.part201Col)
         len2 = self.getArrLenth(self.part201Col, self.part301Col)
