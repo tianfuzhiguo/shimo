6d352c68075b8d1b465b606d8546c78dad81910e
tianfuzhiguo <49770448+tianfuzhiguo@users.noreply.github.com>
2023-06-08 16:47:14 +0800

    Update Report.py

--- common/excel/Report.py
+++ common/excel/Report.py
@@ -1,6 +1,7 @@
 import openpyxl
 import shutil
 import xlrd
+import os
 from xlutils.copy import copy
 from common.utils.ExcelUtil import ExcelUtil
 
@@ -41,3 +42,26 @@ class Report(ExcelUtil):
             fileCheck = f"文件：{fileRes} 正在被其他程序使用"
             print(fileCheck)
             self.setFonts('red', fileCheck)
+            
+    def createHTMLReport(self, js, date, path, file):
+        """
+        创建html测试报告
+        :param path: 文件路径
+        :param file: 文件名
+        :param js: json格式的测试结果
+        @param date: 报告日期
+        """
+        try:
+            html_template = self.resource_path("source/template")
+            with open(html_template, "r", encoding="utf-8") as f:
+                html_data = f.read()
+            html = html_data.replace('${resultData}', str(js))
+            file_name = file[:file.index('.xls')]
+            html_file = f"{path}/result/{file_name}-{date}-report.html"
+            if os.path.exists(html_file):
+                os.remove(html_file)
+            with open(html_file, 'w', encoding='utf-8') as f:
+                f.write(html)
+            return html_file
+        except Exception as e:
+            print(e)
